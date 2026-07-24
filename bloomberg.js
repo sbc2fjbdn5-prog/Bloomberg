83058c9cab254a67b14b5d87341668152fe0b87e
sbc2fjbdn5-prog <sbc2fjbdn5@privaterelay.appleid.com>
2026-07-25 01:25:31 +0800

    Add files via upload

---
+++ bloomberg.js
@@ -0,0 +1,34 @@
+/*************************************
+
+项目名称： Bloomberg
+
+**************************************
+
+[rewrite_local]
+^https:\/\/mobapi\.bloomberg\.com\/wssmobile\/v1\/user\/subscription url script-response-body https://raw.githubusercontent.com/sbc2fjbdn5-prog/Bloomberg/main/bloomberg.js
+[mitm]
+hostname = mobapi.bloomberg.com
+
+*************************************/
+
+if ($response.body) {
+    let obj = JSON.parse($response.body);
+    obj.subscriptions = [{
+        "active": true,
+        "product": "BBG",
+        "provider": "ZUORA",
+        "id": "88888888",
+        "subscriptionNumber": "88888888",
+        "status": "ACTIVE",
+        "endDate": "2099-12-31",
+        "startDate": "2024-01-01T00:00:00",
+        "entitlements": [{
+            "product": "BBG",
+            "type": "DIGITAL"
+        }],
+        "type": "DIGITAL"
+    }];
+    $done({ body: JSON.stringify(obj) });
+} else {
+    $done({});
+}
